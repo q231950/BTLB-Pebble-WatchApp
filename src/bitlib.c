19c9fb2a2e500b04964d882b0ec075886ad084fa
Martin Kim Dung-Pham <kim@elbedev.com>
2015-12-30 22:14:49 -0500

    Push new window when selecting bookmarks/loans

--- src/bitlib.c
+++ src/bitlib.c
@@ -4,94 +4,89 @@
 
 #include <pebble.h>
 
-#define KEY_COUNT 5
+//#define KEY_COUNT 5
+#define NUM_FIRST_MENU_ITEMS 2
 
 static Window *s_main_window;;
 static MenuLayer *s_menu_layer;
-static AppSync s_sync;
-static uint8_t s_sync_buffer[32];
 
-#define NUM_MENU_SECTIONS 2
-#define NUM_FIRST_MENU_ITEMS 3
-#define NUM_SECOND_MENU_ITEMS 1
+//static AppSync s_sync;
+//static uint8_t s_sync_buffer[32];
+//static char ** bookmarks = NULL;
 
 static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
-    return NUM_MENU_SECTIONS;
+    return 1;
 }
 
 static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
     switch (section_index) {
         case 0:
-            return NUM_FIRST_MENU_ITEMS;
-        case 1:
-            return NUM_SECOND_MENU_ITEMS;
+            return 2;
         default:
             return 0;
     }
 }
 
 static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
+    static char title[32];
+    
     switch (cell_index->section) {
         case 0:
             switch (cell_index->row) {
-                default: {
-                    menu_cell_basic_draw(ctx, cell_layer, "Basic Item", "With a subtitle", NULL);
+                case 0:
+                    snprintf(title, sizeof(title), "bookmarks");
+                    break;
+                case 1: {
+                    snprintf(title, sizeof(title), "loans");
                     break;
                 }
-            }
-            break;
-        case 1:
-            switch (cell_index->row) {
                 default:
-                {
-#ifdef PBL_RECT
-                    menu_cell_title_draw(ctx, cell_layer, "Final Item");
-#else
-                    GSize size = layer_get_frame(cell_layer).size;
-                    graphics_draw_text(ctx, "Final Item", fonts_get_system_font(FONT_KEY_GOTHIC_28),
-                                       GRect(0, 0, size.w, size.h), GTextOverflowModeTrailingEllipsis,
-                                       GTextAlignmentCenter, NULL);
-#endif
+                    snprintf(title, sizeof(title), " ");
                     break;
                 }
-            }
+            break;
     }
-}
-
-#ifdef PBL_ROUND
-static int16_t get_cell_height_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *callback_context) {
-    if (menu_layer_is_index_selected(menu_layer, cell_index)) {
-        switch (cell_index->row) {
-            case 0:
-                return MENU_CELL_ROUND_FOCUSED_SHORT_CELL_HEIGHT;
-                break;
-            default:
-                return MENU_CELL_ROUND_FOCUSED_TALL_CELL_HEIGHT;
-        }
-    } else {
-        return MENU_CELL_ROUND_UNFOCUSED_SHORT_CELL_HEIGHT;
-    }
-}
+    
+#ifdef PBL_RECT
+    menu_cell_title_draw(ctx, cell_layer, title);
+#else
+    GSize size = layer_get_frame(cell_layer).size;
+    graphics_draw_text(ctx, title, fonts_get_system_font(FONT_KEY_GOTHIC_28),
+                       GRect(0, 0, size.w, MENU_CELL_ROUND_FOCUSED_SHORT_CELL_HEIGHT), GTextOverflowModeTrailingEllipsis,
+                       GTextAlignmentCenter, NULL);
 #endif
+
+}
 
 static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
-    // Use the row to specify which item will receive the select action
+    Window *newWindow = NULL;
     switch (cell_index->row) {
-        case 1:
+        case 0:
+            APP_LOG(APP_LOG_LEVEL_ERROR, "Select bookmarks");
+            newWindow = window_create()
+            break;
+        default:
+            newWindow = window_create()
             break;
     }
+    
+    window_set_window_handlers(newWindow, (WindowHandlers) {
+        .load = NULL,
+        .unload = NULL,
+    });
+    window_stack_push(newWindow, true);
 }
 
-static void sync_changed_handler(const uint32_t key, const Tuple *new_tuple, const Tuple *old_tuple, void *context) {
-    static char s_bookmarks_buffer[32];
-    snprintf(s_bookmarks_buffer, sizeof(s_count_buffer), "Count: %d", (int)new_tuple->value->int32);
-    //    text_layer_set_text(s_output_layer, s_count_buffer);
-}
-
-static void sync_error_handler(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
-    // An error occured!
-    APP_LOG(APP_LOG_LEVEL_ERROR, "sync error!");
-}
+//static void sync_changed_handler(const uint32_t key, const Tuple *new_tuple, const Tuple *old_tuple, void *context) {
+//    static char s_bookmarks_buffer[32];
+//    snprintf(s_bookmarks_buffer, sizeof(s_bookmarks_buffer), "Count: %d", (int)new_tuple->value->int32);
+//    //    text_layer_set_text(s_output_layer, s_bookmarks_buffer);
+//}
+//
+//static void sync_error_handler(DictionaryResult dict_error, AppMessageResult app_message_error, void *context) {
+//    // An error occured!
+//    APP_LOG(APP_LOG_LEVEL_ERROR, "sync error!");
+//}
 
 // Window
 
@@ -129,16 +124,16 @@ static void init(void) {
     });
     window_stack_push(s_main_window, true);
     
-    // Setup AppSync
-    app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+//     Setup AppSync
+//    app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
     
     // Setup initial values
-    Tuplet initial_values[] = {
-        TupletInteger(KEY_COUNT, 0),
-    };
+//    Tuplet initial_values[] = {
+//        TupletInteger(KEY_COUNT, 0),
+//    };
     
     // Begin using AppSync
-    app_sync_init(&s_sync, s_sync_buffer, sizeof(s_sync_buffer), initial_values, ARRAY_LENGTH(initial_values), sync_changed_handler, sync_error_handler, NULL);
+//    app_sync_init(&s_sync, s_sync_buffer, sizeof(s_sync_buffer), initial_values, ARRAY_LENGTH(initial_values), sync_changed_handler, sync_error_handler, NULL);
 }
 
 static void deinit(void) {
@@ -146,7 +141,7 @@ static void deinit(void) {
     window_destroy(s_main_window);
     
     // Finish using AppSync
-    app_sync_deinit(&s_sync);
+//    app_sync_deinit(&s_sync);
 }
 
 int main(void) {
